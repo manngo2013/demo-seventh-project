779c9de41dbe6d216a7b97db482e0e7f997c7447
manngo <manngo2013@gmail.com>
2023-09-02 22:17:34 +0700

    Lecture 81: Install and Using Redux Devtools

--- src/config/store.js
+++ src/config/store.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, compose, legacy_createStore as createStore } from 'redux'
 import rootReducer from '../reducers/rootReducer';
 import myLogger from '../middlewares/logger';
 
-const store = createStore(rootReducer, {}, applyMiddleware(myLogger));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(myLogger)));
 
 export default store;
